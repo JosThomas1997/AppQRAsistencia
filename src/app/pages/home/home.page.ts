3fb6a316cee414c547483a264214af67e98a85d9
cmontoya1 <c.montoya@duocuc.cl>
2024-09-01 16:46:09 -0400

    Implementé ngModel y ngIf en el login. También 2x2 cards en la homepage con redirección en el ts

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class HomePage implements OnInit {
   username: string = ''; 
 
+  constructor(private router: Router){}
+
+  navigateClases() {
+    this.router.navigate(['/clases']);
+  }
+
+  navigateHorario() {
+    this.router.navigate(['/horarios']);
+  }
+
+  navigateAsistencia() {
+    this.router.navigate(['/asistencia']);
+  }  
+
   ngOnInit() {
     this.username = localStorage.getItem('username') || '';
   }
+
+  
 
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -9,26 +10,53 @@ import { AlertController } from '@ionic/angular';
 })
 
 export class LoginPage implements OnInit {
-  username: string = '';
-  password: string = '';
 
-  constructor(private router: Router, private alertController: AlertController) {}
-
-  async login() {
-    if (this.username === 'duoc' && this.password === 'duoc') {
-      localStorage.setItem('username', this.username);
-      this.router.navigateByUrl('/home');
-    } else {
-      const alert = await this.alertController.create({
-        header:'ERROR',
-        message: 'USUARIO O CONTRASEÑA INCORRECTA',
-        buttons: ['Entendido']
-      });
-      await alert.present();
-    }
+  constructor(private router: Router, private formBuilder:FormBuilder, private alertController:AlertController, private loadingController:LoadingController) {
+    this.loginForm = this.formBuilder.group({
+      email : ['', [Validators.required, Validators.email]],
+      pass : ['', [Validators.required, Validators.minLength(1)]]
+    })
   }
+
+  loginForm: FormGroup;
+  emailValue?: string;
+  passValue?: string;
+
 
   ngOnInit() {
   }
 
-}
+  async login () {
+    const loading = await this.loadingController.create({
+      message: 'Cargando...',
+      duration : 200
+    });
+
+
+    const email = this.emailValue;
+  const pass = this.passValue;
+  if (email == '1' && pass== '1'){
+    await loading.present();
+    localStorage.setItem('usuarioLogin',email);
+    setTimeout(async() => {
+      await loading.dismiss();
+      this.router.navigate(['home']);
+    }, 2000)
+    
+  } else{
+    const alert = await this.alertController.create({
+      header: 'Acceso denegado',
+      message: 'Usuario o contraseña incorrecta.',
+      buttons: ['OK']
+    });
+    await alert.present();
+    this.emailValue = '';
+    this.passValue = '';
+  }
+  
+ } 
+
+
+  }
+
+
